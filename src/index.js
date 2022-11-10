531ec3bc81cf3dc33ccc7fd02cd4de8d2ba2e7a7
Alycia Canavan <alycia.canavan@gmail.com>
2022-11-09 20:20:23 -0800

    Wrapped WineProvider around app (inside ScoreProvider)

--- src/index.js
+++ src/index.js
@@ -11,7 +11,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Router>
     <ScoreProvider>
-        <App />
+      <WineProvider>
+          <App />
+      </WineProvider>
     </ScoreProvider>
   </Router>
 );
